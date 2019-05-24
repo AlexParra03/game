f6a7e0fcce953eede4125890defe80a4b74c1667
David Alejandro Parra Moreno <alex.user03@gmail.com>
2019-05-24 16:42:09 -0400

    Fixed bug that render tilemap rotated 90 degrees

--- client/src/GameObjects/TileMap.ts
+++ client/src/GameObjects/TileMap.ts
@@ -3,19 +3,20 @@ import { GameObject } from "./GameObject";
 export class TileMap extends GameObject {
     readonly TILE_SIZE = 40;
     readonly WIDTH = 15;
-    readonly HEIGHT = 15;
+    readonly HEIGHT = 16;
     readonly MAP = [
+        [1,1,1,1,1,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [1,1,1,1,1,0,0,0,0,0,0,0,0,0,0],
+        [1,1,1,1,1,0,0,0,0,0,0,0,0,0,0],
+        [1,1,1,1,1,0,0,0,0,0,0,0,0,0,0],
+        [1,1,1,1,1,0,0,0,0,0,0,0,0,0,0],
+        [1,1,1,1,1,0,0,0,0,0,0,0,0,0,0],
+        [1,1,1,1,1,0,0,0,0,0,0,0,0,0,0],
+        [1,1,1,1,1,0,0,0,0,0,0,0,0,0,0],
+        [1,1,1,1,1,0,0,0,0,0,0,0,0,0,0],
         [1,1,1,1,1,1,1,1,1,1,1,1,0,0,0],
         [1,1,1,1,1,1,1,1,1,1,1,1,0,0,0],
         [1,1,1,1,1,1,1,1,1,1,1,1,0,0,0],
@@ -27,11 +28,11 @@ export class TileMap extends GameObject {
         textures.push( PIXI.Texture.fromImage('assets/graphics/Tiles/grass20.png', true));
         textures.push( PIXI.Texture.fromImage('assets/graphics/Tiles/water.png', true));
 
-        for(let row = 0; row < this.WIDTH; row++) {
-            for(let col = 0; col < this.HEIGHT; col++) {
+        for(let row = 0; row < this.HEIGHT; row++) {
+            for(let col = 0; col < this.WIDTH; col++) {
                 let sprite = new PIXI.Sprite(textures[this.MAP[row][col]]);
-                sprite.position.x = row * this.TILE_SIZE;
-                sprite.position.y = col * this.TILE_SIZE;
+                sprite.position.x = col * this.TILE_SIZE;
+                sprite.position.y = row * this.TILE_SIZE;
 
                 this.addChild(sprite);
             }
